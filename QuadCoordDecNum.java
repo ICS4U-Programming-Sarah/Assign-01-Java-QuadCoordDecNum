8d7d6c326ec3a6a83a8758eaca4963802c94424d
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-03-05 00:42:49 +0000

    lala

--- QuadCoordDecNum.java
+++ QuadCoordDecNum.java
@@ -33,23 +33,28 @@ public final class QuadCoordDecNum {
     public static void main(String[] args) {
         // Introduction.
         System.out.println("Today, we will be two problems.");
-        System.out.println("Problem 1: Determining quadrant based on  x & y coordinates.");
-        System.out.println("Problem 2: Decimal number on basis of input.");
+        System.out.print("Problem 1: Determining quadrant based ");
+        System.out.println("on x & y coordinates.");
+        System.out.print("Problem 2: Decimal number placement on");
+        System.out.println(" on basis of user input.");
 
-        // Declare variables. 
+        // Declare variables.
         final String opOne = "1";
+        final double zeroOp = 0;
+        final String opTwo = "2";
 
         // Creates a new scanner
         final Scanner scanner = new Scanner(System.in);
-        
+
         // Get user input.
         System.out.println("Which one would you like to do? (1 or 2): ");
         final String userSelector = scanner.nextLine();
-        
+
         // If statement to see which option user selects.
         if (userSelector.equals(opOne)) {
 
             // Receive user input.
+            System.out.println("");
             System.out.print("Please enter the (x) coordinate: ");
             final String xCoordStr = scanner.nextLine();
             // Try catch to detect error.
@@ -65,12 +70,36 @@ public final class QuadCoordDecNum {
                     // Convert from string to double.
                     final double yCoordDoub = Double.parseDouble(yCoordStr);
 
-                    if (xCoordDoub > 0 && yCoordDoub > 0) {
-                        System.out.print("For the point " + xCoordStr + yCoordDoub +
-                            "it lies in the first quadrant.");
+                    // If statement to determine quadrant on basis
+                    // of user input. It then displays the corresponding.
+                    // quadrant.
+                    if (xCoordDoub > zeroOp && yCoordDoub > zeroOp) {
+                        System.out.print("For the point, (" + xCoordStr
+                                + ", " + yCoordDoub + ")");
+                        System.out.println(", it lies in the first quadrant.");
+                    } else if (xCoordDoub < zeroOp && yCoordDoub > zeroOp) {
+                        System.out.print("For the point, (" + xCoordStr
+                                + ", " + yCoordDoub + ")");
+                        System.out.println(", it lies in the second quadrant.");
+                    } else if (xCoordDoub < zeroOp && yCoordDoub < zeroOp) {
+                        System.out.print("For the point, (" + xCoordStr
+                                + ", " + yCoordDoub + ")");
+                        System.out.println(", it lies in the third quadrant.");
+                    } else if (xCoordDoub > zeroOp && yCoordDoub < zeroOp) {
+                        System.out.print("For the point, (" + xCoordStr
+                                + ", " + yCoordDoub + ")");
+                        System.out.println(", it lies in the fourth quadrant.");
+                    } else if (xCoordDoub == zeroOp && yCoordDoub == zeroOp) {
+                        System.out.print("For the point, (" + xCoordStr
+                                + ", " + yCoordDoub + ")");
+                        System.out.println(" it lies in the origin.");
+                    } else {
+                        // Display error.
+                        System.out.print("Please enter a valid coordinate(s).");
                     }
-                    
+
                 } catch (NumberFormatException error) {
+                    // Displays error to user.
                     System.out.print("Refrain from invalid input."
                             + error.getMessage());
                 }
@@ -80,10 +109,58 @@ public final class QuadCoordDecNum {
                 System.out.print("Please enter valid input."
                         + error.getMessage());
             }
+            // Executes when user selects option two.
+        } else if (userSelector.equals(opTwo)) {
+            System.out.print("This program tells you if the two");
+            System.out.print(" decimals numbers entered are up to the same");
+            System.out.println(" decimal places!");
 
-        } 
-    
+            // Receive user input.
+            System.out.print("Enter the first decimal number: ");
+            final String numOneStr = scanner.nextLine();
 
+            // Usage of try catch to detect errors.
+            try {
+                // Convert from string to double.
+                final double numOneDoub = Double.parseDouble(numOneStr);
+
+                // Receive user input.
+                System.out.print("Enter the second decimal number: ");
+                final String numTwoStr = scanner.nextLine();
+
+                try {
+                    // Convert from string to double.
+                    final double numTwoDoub = Double.parseDouble(numTwoStr);
+
+                    // Round to 3 decimal places.
+                    System.out.format("%.3f", numOneDoub);
+                    System.out.format("%.3f", numTwoDoub);
+
+                    // If statement to see if numbers are up to
+                    // same decimal places.
+
+                    if (numOneDoub == numTwoDoub) {
+                        System.out.print("The numbers are up to the same decimal places.");
+                    } else {
+                        System.out.print("The numbers aren't up to same decimal places.");
+                    }
+
+                } catch (NumberFormatException error) {
+                    // Displays error to user.
+                    System.out.print("Valid inputs only!"
+                            + error.getMessage());
+                }
+            } catch (NumberFormatException error) {
+                // Displays error to user.
+                System.out.print("Invalid, please enter valid input."
+                        + error.getMessage());
+            }
+            // Executes if user does not select one of the options.
+        } else {
+            System.out.print("Please select either or option.");
+        }
+        scanner.close();
 
     }
 }
+
